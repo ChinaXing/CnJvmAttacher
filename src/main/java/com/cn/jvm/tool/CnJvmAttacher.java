c04b29f743ed1a7bc61907dc8f64ce7e904c5d3f
chinaxing <chen.yack@gmail.com>
2014-10-10 14:24:28 +0800

    -- add resource --

---
+++ src/main/java/com/cn/jvm/tool/CnJvmAttacher.java
@@ -0,0 +1,28 @@
+package com.cn.jvm.tool;
+
+import com.sun.tools.attach.VirtualMachine;
+
+/**
+ * Created by ChinaXing on 14-10-10.
+ */
+public class CnJvmAttacher {
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("usage : java -cp /path/to/jdk/lib/tools.jar:. attachPID agentJarFileAbsolutePath agentArgumentsString");
+            System.out.println("eg: ");
+            System.out.println("2022 /root/CnAgent.jar \"com.apple.concurrent.Dispatch::getInstance System.out.println(\\\"ok\\\");\"");
+            return;
+        }
+        try {
+            Integer pid = Integer.parseInt(args[0]);
+            String agentJar = args[1];
+            VirtualMachine vm = VirtualMachine.attach(pid.toString());
+            vm.loadAgent(agentJar, args[2]);
+            System.out.println("Load agent ok");
+        } catch (Exception e) {
+            System.err.println("error:" + e.getMessage());
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
