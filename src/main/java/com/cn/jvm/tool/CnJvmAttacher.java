5106007f4988b79f5abef0da1bde57923c50fafd
乔玄 <qiaoxuan@mogujie.com>
2014-11-04 14:02:38 +0800

    ok

--- src/main/java/com/cn/jvm/tool/CnJvmAttacher.java
+++ src/main/java/com/cn/jvm/tool/CnJvmAttacher.java
@@ -8,12 +8,15 @@ import com.sun.tools.attach.VirtualMachine;
 public class CnJvmAttacher {
     public static void main(String[] args) {
         if (args.length < 3) {
-            System.out.println("usage : java -cp /path/to/jdk/lib/tools.jar:. attachPID agentJarFileAbsolutePath agentArgumentsString");
+            System.out.println("usage : java -cp /path/to/jdk/lib/tools.jar:{thisJar}:. com.cn.jvm.tool.CnJvmAttacher attachPID agentJarFileAbsolutePath agentArgumentsString");
             System.out.println("eg: ");
-            System.out.println("2022 /root/CnAgent.jar \"com.apple.concurrent.Dispatch::getInstance System.out.println(\\\"ok\\\");\"");
+            System.out.println("2022 /root/CnAgent.jar 'com.apple.concurrent.Dispatch::getInstance|System.out.println(\"ok\");'");
             return;
         }
         try {
+            System.out.println("Agent Jar file  : " + args[1]);
+            System.out.println("Agent Arguments : " + args[2]);
+
             Integer pid = Integer.parseInt(args[0]);
             String agentJar = args[1];
             VirtualMachine vm = VirtualMachine.attach(pid.toString());
